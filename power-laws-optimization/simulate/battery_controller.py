5247644db0aaedc77dc044e97c09b72d121f9dde
Yunlin Zhang <yunlinz@gmail.com>
2018-03-07 22:00:02 -0500

    ac action taker done

--- power-laws-optimization/simulate/battery_controller.py
+++ power-laws-optimization/simulate/battery_controller.py
@@ -1,11 +1,7 @@
 import tensorflow as tf
 from tensorflow.contrib import slim
-
-from keras.layers import Input, LSTM, Concatenate, Dense, Conv1D
-from keras.models import Model
-import keras as K
-import multiprocessing 
-import threading
+from tensorflow.contrib.distributions import Beta
+from battery import Battery
 
 import numpy as np
 
@@ -13,7 +9,7 @@ import numpy as np
 #    THIS CLASS WILL BE IMPLEMENTED BY COMPETITORS
 # ==============================================================================
 class ActorCriticNetwork(object):
-  def __init__(self, scope, trainer, augment_size=3,
+  def __init__(self, trainer, scope='global', augment_size=41,
                forecast_size=96, forecast_dim=4, rnn_a_size=24, fc_size=16):
     self.inputs = tf.placeholder(shape=[None, forecast_size, forecast_dim], dtype=tf.float32)
     lstm_cell = tf.contrib.rnn.BasicLSTMCell(rnn_a_size, state_is_tuple=True)
@@ -26,16 +22,25 @@ class ActorCriticNetwork(object):
     state_in = tf.contrib.rnn.LSTMStateTuple(c_in, h_in)
     lstm_output, _ = tf.nn.dynamic_rnn(lstm_cell, self.inputs, initial_state=state_in)
     rnn_out = tf.reshape(lstm_output[:,-1,:], [-1, rnn_a_size])
-    augmented_input = tf.concat([rnn_out, tf.placeholder(shape=[None, augment_size], dtype=tf.float32)], axis=1)
+    self.curr_state = tf.placeholder(shape=[None, augment_size], dtype=tf.float32)
+    augmented_input = tf.concat([rnn_out, self.curr_state], axis=1)
 
-    policy_dense = slim.fully_connected(augmented_input, 16,
+    policy_dense = slim.fully_connected(augmented_input, fc_size,
                                        activation_fn=tf.nn.relu,
                                        weights_initializer=tf.glorot_normal_initializer)
-    self.policy = slim.fully_connected(policy_dense, 2,
+    self.policy_alpha = tf.exp(slim.fully_connected(policy_dense, 1,
+                                       activation_fn=tf.nn.relu,
+                                       weights_initializer=tf.glorot_normal_initializer))
+    self.policy_beta = tf.exp(slim.fully_connected(policy_dense, 1,
+                                       activation_fn=tf.nn.relu,
+                                       weights_initializer=tf.glorot_normal_initializer))
+
+    self.policy = (self.policy_alpha - 1) / (self.policy_alpha + self.policy_beta - 2)
+    value_dense = slim.fully_connected(augmented_input, fc_size,
                                        activation_fn=tf.nn.relu,
                                        weights_initializer=tf.glorot_normal_initializer)
 
-    self.value = slim.fully_connected(augmented_input, 1,
+    self.value = slim.fully_connected(value_dense, 1,
                                       activation_fn=None,
                                       weights_initializer=tf.glorot_normal_initializer)
 
@@ -44,8 +49,10 @@ class ActorCriticNetwork(object):
     self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
     
     self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.target_v - self.value))
-    self.policy_loss = - tf.reduce_sum(self.actions * self.advantages) # TODO what is the policy loss supposed to be?
-    self.loss = 0.5 * self.value_loss + self.policy_loss
+
+    self.policy_loss = - tf.reduce_sum(tf.log(self.policy) * self.advantages) # TODO what is the policy loss supposed to be?
+    self.entropy = Beta(self.policy_alpha, self.policy_beta).entropy()
+    self.loss = 0.5 * self.value_loss + self.policy_loss + 0.001 * self.entropy
 
     local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
     self.gradients = tf.gradients(self.loss, local_vars)
@@ -86,8 +93,9 @@ class BatteryContoller(object):
       :returns: proposed state of charge, a float between 0 (empty) and 1 (full).
   """
   def __init__(self):
+    self.network = ActorCriticNetwork(tf.train.AdamOptimizer())
     self.session = tf.Session()
-    pass
+    self.session.run(tf.global_variables_initializer())
 
   def propose_state_of_charge(self,
                               site_id,
@@ -101,5 +109,35 @@ class BatteryContoller(object):
                               pv_forecast):
 
     # return the proposed state of charge ...
-    
-    return 1.0
+    forecast = self.make_forecast_array(price_buy, price_sell, load_forecast, pv_forecast, battery)
+    out_inputs = self.make_other_inputs(battery, timestamp, actual_previous_load, actual_previous_pv_production)
+
+    return self.get_action(forecast, out_inputs)
+
+  def get_action(self, forecast, state):
+    policy_output = self.session.run(self.network.policy,
+                                     feed_dict={self.network.inputs:forecast,
+                                                self.network.curr_state:state})
+    return policy_output
+
+  def make_forecast_array(self, price_buy, price_sell, load_forecast, pv_forecast,
+                          battery: Battery):
+    forecast_size = len(price_buy)
+    forecast_array = np.zeros((1, forecast_size, 4))
+    forecast_array[0,:,0] = price_buy - price_sell # premium
+    forecast_array[0,:,1] = price_buy + price_sell # spread
+    forecast_array[0,:,2] = load_forecast * (15/60) / (battery.capacity * battery.discharging_efficiency)
+    forecast_array[0,:,3] = pv_forecast * (15/60)  / battery.capacity * battery.discharging_efficiency # not a typo, efficiency is treated differently for charge and discharge
+    return forecast_array
+
+  def make_other_inputs(self, battery: Battery, timestamp, actual_previous_load, actual_previous_pv_production):
+    input_array = np.zeros((1, 5 + 12 + 24))
+    input_array[0, 0] = battery.current_charge
+    input_array[0, 1] = battery.charging_power_limit / battery.capacity
+    input_array[0, 2] = battery.discharging_power_limit / battery.capacity
+    input_array[0, 3] = actual_previous_load
+    input_array[0, 4] = actual_previous_pv_production
+    input_array[0, 5 + timestamp.month] = 1.0
+    input_array[0, 17 + timestamp.hour] = 1.0
+    return input_array
+
